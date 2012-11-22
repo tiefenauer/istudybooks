cf00b8ce9aa8489c260b99841af4182d7d1d7e27
Knobli <raffael@santschi.ch>
2012-11-22 22:58:53 +0100

    test

--- src/main/php/webapp/application/controllers/book.php
+++ src/main/php/webapp/application/controllers/book.php
@@ -6,59 +6,61 @@ class Book extends CI_Controller{
 	}
 	
 	public function book_insert(){
-		//find out article type id
-		$query = $this->db->query('SELECT pk_articletype FROM tbl_articletype where typename = "Buch"');
-		$typeId = $query->result()->pk_articletype;
-		
-		//insert article
-		$data = array(
-               'fk_articletype' => $typeId,
-        );				
-		$this->db->insert('tbl_article', $data);
-		$articleId = $this->db->insert_id();
-		
-		//insert book
-		if(!empty($POST['name'])){
-			$name = $POST['name'];
-		} else {
-			$name = "";
-		}
-		if(!empty($POST['author'])){
-			$author = $POST['author'];
-		} else {
-			$author = "";
-		}
-		if(!empty($POST['isbn'])){
-			$isbn = $POST['isbn'];
-		} else {
-			$isbn = "";
-		}
-		if(!empty($POST['picture'])){
-			$picture = $POST['picture'];
-		} else {
-			$picture = "";
-		}						
-		$data = array(
-               'fk_article' => $articleId,
-               'name' => $name,
-               'author' => $author,
-               'ISBN' => $isbn,
-               'picture' => $picture,
-        );
-		$this->db->insert('tbl_book', $data);
-		
-		//insert offert
-		if(!empty($POST['price'])){
-			$price = $POST['price'];
-		} else {
-			$price = "";
-		}	
-		$bookId = $this->db->insert_id();
-		$data = array(
-               'fk_article' => $bookId,
-               'price' => $price,
-        );		
-		$this->db->insert('tbl_offer',$data);
+		echo "Form post:<br>";
+		print_r($_POST);
+		// //find out article type id
+		// $query = $this->db->query('SELECT pk_articletype FROM tbl_articletype where typename = "Buch"');
+		// $typeId = $query->result()->pk_articletype;
+// 		
+		// //insert article
+		// $data = array(
+               // 'fk_articletype' => $typeId,
+        // );				
+		// $this->db->insert('tbl_article', $data);
+		// $articleId = $this->db->insert_id();
+// 		
+		// //insert book
+		// if(!empty($_POST['name'])){
+			// $name = $_POST['name'];
+		// } else {
+			// $name = "";
+		// }
+		// if(!empty($_POST['author'])){
+			// $author = $_POST['author'];
+		// } else {
+			// $author = "";
+		// }
+		// if(!empty($_POST['isbn'])){
+			// $isbn = $_POST['isbn'];
+		// } else {
+			// $isbn = "";
+		// }
+		// if(!empty($_POST['picture'])){
+			// $picture = $_POST['picture'];
+		// } else {
+			// $picture = "";
+		// }						
+		// $data = array(
+               // 'fk_article' => $articleId,
+               // 'name' => $name,
+               // 'author' => $author,
+               // 'ISBN' => $isbn,
+               // 'picture' => $picture,
+        // );
+		// $this->db->insert('tbl_book', $data);
+// 		
+		// //insert offert
+		// if(!empty($_POST['price'])){
+			// $price = $_POST['price'];
+		// } else {
+			// $price = "";
+		// }	
+		// $bookId = $this->db->insert_id();
+		// $data = array(
+               // 'fk_article' => $bookId,
+               // 'price' => $price,
+        // );		
+		// $this->db->insert('tbl_offer',$data);
 	}
 } 
 ?>
