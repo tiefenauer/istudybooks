1d910cbc2a6a19a33e9b79021842c7618d77bc8f
klickagent <hello@klickagent.ch>
2012-11-26 16:09:46 +0100

    added google analytics

--- src/main/php/webapp/application/views/include/footer.php
+++ src/main/php/webapp/application/views/include/footer.php
@@ -3,5 +3,26 @@
 	    <p>Designed and built by Daniel Tiefenauer, Roman Lickel and Raffael Santschi using CodeIgniter, Bootstrap and jquery</p>
 	</footer>
 </div>	
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+<script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+<script src="<?= base_url('js/bootstrap-datepicker.js') ?>"></script>
+   
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36135351-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
 </body>
 </html>
